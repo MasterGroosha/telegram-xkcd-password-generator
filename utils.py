abebf0fba2beb58a3b7705f497ad62f44c1c73f0
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-05-23 17:03:18 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,12 +2,4 @@
 
 
 def get_language(lang_code):
-    # Иногда language_code может быть None
-    if not lang_code:
-        return "en"
-    if "-" in lang_code:
-        lang_code = lang_code.split("-")[0]
-    if lang_code == "ru":
-        return "ru"
-    else:
-        return "en"
+    return lang_code.split("-")[0] if lang_code else 'en'
