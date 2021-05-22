f997bac86dd6c4060dd9afb3378437e1a20a281b
Groosha <groosha@protonmail.com>
2021-05-22 13:38:59 +0300

    Fixed %, delimeter

--- bot/pwdgen.py
+++ bot/pwdgen.py
@@ -4,7 +4,7 @@ from xkcdpass import xkcd_password
 
 class XKCD:
     delimiters_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    delimiters_full = ["!", "$", "%,", "^", "&", "*", "-", "_", "+", "=",
+    delimiters_full = ["!", "$", "%", "^", "&", "*", "-", "_", "+", "=",
                        ":", "|", "~", "?", "/", ".", ";"] + delimiters_numbers
 
     def __init__(self, filename: str):
